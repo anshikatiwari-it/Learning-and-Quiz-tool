41a0ce203d7ea4a1643097172aa9e08d89e0616e
kanishkatiwari23 <kaniskatiwari23@gmail.com>
2026-07-15 15:45:11 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -65,3 +65,35 @@ def login():
         return "Invalid Login"
 
     return render_template("login.html")
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/learn")
+def learn():
+    return render_template("learn.html")
+
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html")
+
+@app.route("/result")
+def result():
+    return render_template("result.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
